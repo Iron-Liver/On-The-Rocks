61a428e027e8cced3021a01747b1bb55b54c386b
marlopc <lucaspanaro1@gmail.com>
2021-08-02 18:50:11 -0300

    branch init

---
+++ api/src/models/Order.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("order", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    total: {
+      type: DataTypes.FLOAT(10, 2),
+    },
+  });
+};
