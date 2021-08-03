1152f6d93b62e6b2b9a32bcbaf6b097214c007c1
marlopc <lucaspanaro1@gmail.com>
2021-08-03 19:13:59 -0300

    creates controllers - minor changes to model

---
+++ api/src/controllers/order/updateOrderState.js
@@ -0,0 +1,21 @@
+const { Order } = require("../../db");
+
+module.exports = async (req, res, next) => {
+  const { orderId, newState } = req.body;
+  
+  try {
+    const [ updated ] = await Order.update({state: newState}, { 
+      where: {
+        id: orderId
+      }
+    });
+
+    if(!updated) {
+      throw new Error(`Order ${orderId} not found / has not been modified`)
+    }
+
+    return res.send(`Order ${orderId} successfully updated`).status(200);
+  } catch (err) {
+    return res.send({error: err.message}).status(409);
+  }
+};
