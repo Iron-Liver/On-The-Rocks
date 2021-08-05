3559e13061524acd63e7f7947591b3e3b1df72d1
Federico <solamentefedee@gmail.com>
2021-08-05 13:09:04 -0300

    updateProduct route created

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const express = require("express");
 
 const createProduct = require("../controllers/product/createProduct");
-//const putProduct = require("../controllers/product/updateProduct");
+const putProduct = require("../controllers/product/updateProduct");
 const deleteProduct = require("../controllers/product/deleteProduct");
 const getAllProducts = require("../controllers/product/getProduct");
 const getProductById = require("../controllers/product/getProductById");
@@ -11,7 +11,7 @@ router.use(express.json());
 router.get('/', getAllProducts);
 router.get('/:id', getProductById);
 router.post('/add', createProduct);
-//router.put('/:id', putProduct);
+router.put('/:id', putProduct);
 router.delete('/:id', deleteProduct); 
 
 module.exports = router;
