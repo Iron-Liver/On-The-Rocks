4d619fa00ccfd3e70fd213475a116f321125e95c
Drivello <Nico_dd@outlook.com.ar>
2021-08-07 05:26:04 -0300

    adding forgotten links to mobile menu

--- client/src/Components/NavBar/drawerLists.jsx
+++ client/src/Components/NavBar/drawerLists.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import {Divider,List, ListItem, ListItemText, TextField} from '@material-ui/core'
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import {LocalBar, Receipt, Star, ListAlt, Business, Search, AccountCircle, Loyalty} from '@material-ui/icons'
@@ -31,58 +32,72 @@ export const MenuList = () => {
           <div className={classes.toolbar}>OnTheRocks</div>
           <Divider />
           <List component="nav">
-            <ListItem button>
-              <ListItemIcon>
-                <LocalBar />
-              </ListItemIcon>
-              <ListItemText primary="Products" />
-            </ListItem>
+            <Link to="/products" style={{textDecoration: 'none', color: 'black'}}>
+              <ListItem button>
+                <ListItemIcon>
+                  <LocalBar />
+                </ListItemIcon>
+                <ListItemText primary="Products" />
+              </ListItem>
+            </Link>
 
-            <ListItem button>
-              <ListItemIcon>
-                <Loyalty />
-              </ListItemIcon>
-              <ListItemText primary="On Sale" />
-            </ListItem>
+            <Link to="/sale" style={{textDecoration: 'none', color: 'black'}}>
+              <ListItem button>
+                <ListItemIcon>
+                  <Loyalty />
+                </ListItemIcon>
+                <ListItemText primary="On Sale" />
+              </ListItem>
+            </Link>
 
-            <ListItem button>
-              <ListItemIcon>
-                <ListAlt />
-              </ListItemIcon>
-              <ListItemText primary="Categories" />
-            </ListItem>
+            <Link to="/categories" style={{textDecoration: 'none', color: 'black'}}>
+              <ListItem button>
+                <ListItemIcon>
+                  <ListAlt />
+                </ListItemIcon>
+                <ListItemText primary="Categories" />
+              </ListItem>
+            </Link>
+
+            <Divider />
+
+            <Link to="/orders" style={{textDecoration: 'none', color: 'black'}}>
+              <ListItem button>
+                <ListItemIcon>
+                  <Receipt />
+                </ListItemIcon>
+                <ListItemText primary="My Orders" />
+              </ListItem>
+            </Link>
+
+            <Link to="/wishlist" style={{textDecoration: 'none', color: 'black'}}>
+              <ListItem button>
+                <ListItemIcon>
+                  <Star />
+                </ListItemIcon>
+                <ListItemText primary="My Wishlist" />
+              </ListItem>
+            </Link>
+
+            <Link to="/profile" style={{textDecoration: 'none', color: 'black'}}>
+              <ListItem button>
+                <ListItemIcon>
+                  <AccountCircle />
+                </ListItemIcon>
+                <ListItemText primary="My Profile" />
+              </ListItem>
+            </Link>
 
             <Divider />
 
-            <ListItem button>
-              <ListItemIcon>
-                <Receipt />
-              </ListItemIcon>
-              <ListItemText primary="My Orders" />
-            </ListItem>
-    
-            <ListItem button>
-              <ListItemIcon>
-                <Star />
-              </ListItemIcon>
-              <ListItemText primary="My Wishlist" />
-            </ListItem>
-    
-            <ListItem button>
-              <ListItemIcon>
-                <AccountCircle />
-              </ListItemIcon>
-              <ListItemText primary="My Account" />
-            </ListItem>
-
-            <Divider />
-    
-            <ListItem button>
-              <ListItemIcon>
-                <Business />
-              </ListItemIcon>
-              <ListItemText primary="About Us" />
-            </ListItem>
+            <Link to="/OnTheRocks" style={{textDecoration: 'none', color: 'black'}}>
+              <ListItem button>
+                <ListItemIcon>
+                  <Business />
+                </ListItemIcon>
+                <ListItemText primary="About Us" />
+              </ListItem>
+            </Link>
           </List>
         </div>
       );
