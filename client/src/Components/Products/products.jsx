2c09c9cfa1a243da7fcd76149d5b6c076749db35
Moroni82 <agus-234@hotmail.com>
2021-08-15 18:58:20 -0300

    landing-products -css

--- client/src/Components/Products/products.jsx
+++ client/src/Components/Products/products.jsx
@@ -1,51 +1,16 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from "react-redux"
 import { getProducts } from '../../Redux/Products/productsActions'
-import { makeStyles } from "@material-ui/core/styles";
+import  './products.css';
 import { Card, CardActionArea, CardContent, CardMedia, Typography, Grid } from "@material-ui/core";
 import { NavLink } from 'react-router-dom';
 import Filters from './Filters';
-
-
-const useStyles = makeStyles({
-    root: {
-        minWidth: 250,
-        maxWidth: 345,
-        height: 460,
-        margin: 10,
-        boxShadow: '0px 3px 1px 1px grey',
-        border: '1px solid grey',
-        '&:hover': {
-            boxShadow: '0px 3px 1px 1.5px #707b7c',
-            transition:'box-shadow 0.1s'
-
-        }
-    },
-    media: {
-       
-        height: 250,
-        width: 200,
-        margin: "auto"
-    },
-    wrapped: {
-        justifyContent: "center",
-        display: "flex",
-        flexWrap: "wrap",
-        margin: 45,
-        marginTop: "3%",
-    },
-    links: {
-        textDecoration: 'none',
-        color: 'black'
-    }
-});
 
 
 const Products = () => {
 
     const {FoundProds, Products} = useSelector(state => state.productReducer)
     const dispatch = useDispatch()
-    const classes = useStyles();
     const {search} = window.location
 
     const products = search.includes('?search') 
@@ -59,47 +24,67 @@ const Products = () => {
     []);
 
     return (
-        <div>
-            <Grid container>
-                <Grid item xs={2}>
-                    <Filters/>
-                </Grid>
-                <Grid>
-                    <div className={classes.wrapped}>
-                        {products?.map((spirits) => {
-                            return (
-                                <Grid key={spirits.id} item md={3} sm={6} xs={12}>
-                                    <Card className={classes.root} >
-                                        <NavLink to={`/products/${spirits.id}`} className={classes.links}>
-                                            <CardActionArea>
-                                                <CardMedia
-                                                    className={classes.media}
-                                                    image={spirits.image}
-                                                    title={spirits.name}
-                                                />
-                                                <CardContent>
-                                                    <Typography gutterBottom variant="h6" component="h6">
-                                                        {spirits.name}
-                                                    </Typography>
-                                                    <Typography variant="body2" color="textSecondary" component="p">
-                                                        {spirits.brand}
-                                                    </Typography>
-                                                    <Typography gutterBottom variant="h6" component="h6">
-                                                        ${spirits.price}
-                                                    </Typography>
-                                                </CardContent>
-                                            </CardActionArea>
-                                        </NavLink>
-                                    </Card>
-
-                                </Grid>
-                            )
-                        })}
+      <div>
+      <div className="filters">
+        <Filters/>
+      </div>
+            <div className="content">
+                {products.map(spirits => {
+                    return (
+                    <div>
+                        <div className="containerproducts">
+                                <div className="card">
+                                  <div className="imgBx">
+                                    <img src={spirits.image} alt="Licorimage"/>
+                                  </div>
+                                  <div className="contentBx">
+                                    <h4>{spirits.name}</h4>
+                                    <div className="size">
+                                        {/* <p>{spirits.brand}</p>  */}
+                                      <h5>{spirits.name}</h5>
+                                    </div>
+                                    <div className="color">
+                                      <h5>${spirits.price}</h5>
+                                    <a href="#">Add to cart</a>
+                                    </div>
+                                  </div>
+                                </div>
+                            </div>
                     </div>
-                </Grid>
-            </Grid>
-        </div>
+                  
+                    )
+                })
+                }
+            </div>
+            </div>
+
     )
 }
 
 export default Products
+
+                                {/* // <Grid key={spirits.id} item md={3} sm={6} xs={12}>
+                                //     <Card className={classes.root} >
+                                //         <NavLink to={`/products/${spirits.id}`} className={classes.links}>
+                                //             <CardActionArea>
+                                //                 <CardMedia
+                                //                     className={classes.media}
+                                //                     image={spirits.image}
+                                //                     title={spirits.name}
+                                //                 />
+                                //                 <CardContent>
+                                //                     <Typography gutterBottom variant="h6" component="h6">
+                                //                         {spirits.name}
+                                //                     </Typography>
+                                //                     <Typography variant="body2" color="textSecondary" component="p">
+                                //                         {spirits.brand}
+                                //                     </Typography>
+                                //                     <Typography gutterBottom variant="h6" component="h6">
+                                //                         ${spirits.price}
+                                //                     </Typography>
+                                //                 </CardContent>
+                                //             </CardActionArea>
+                                //         </NavLink>
+                                //     </Card>
+
+                                // </Grid> */}
