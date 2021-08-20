53c7faa23f5d26ccb5aa9d9b210c151c4195b30d
mativegal <matiasvegalera@gmail.com>
2021-08-20 16:31:37 -0300

    Bugs Fixed & Styles

--- api/src/controllers/wishlist/getId.js
+++ api/src/controllers/wishlist/getId.js
@@ -4,9 +4,9 @@ module.exports = async (req, res, next) => {
   let { id } = req.query;
   try {
     let wishlist = await Wishlist.findAll();
-    console.log("esteee", wishlist);
+    
     let filter = wishlist.filter((e) => e.userId == id);
-    /* let parse = filter.parseIn() */
+    
 
     return res.json(filter);
   } catch (err) {
