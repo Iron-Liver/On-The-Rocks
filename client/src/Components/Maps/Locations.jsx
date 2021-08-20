4e02b584562d720b4726d55a4c1fe9b62e46d1a7
David <david_melo10@yahoo.es>
2021-08-20 11:41:41 -0500

    create a map component

--- client/src/Components/Maps/Locations.jsx
+++ client/src/Components/Maps/Locations.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import "./Locations.css"
 
 const Locations = () => {
     return (
@@ -7,8 +6,9 @@ const Locations = () => {
             <div>
                 <h1>On the Rocks</h1>
                 <h3>branch offices</h3>
-            </div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4197352.453042312!2d-77.66777766056988!3d3.520964844897855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a6f0cc4bb3f1%3A0x1f0fb5e952ae6168!2sCali%2C%20Valle%20del%20Cauca!5e0!3m2!1ses!2sco!4v1629404753504!5m2!1ses!2sco" title="Location" width="100%" height="400" style={{ border: 0}} allowfullscreen="" loading="lazy"></iframe>
+            </div>        
+            
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.710382262643!2d-76.52441068517888!3d3.420561352377885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a6c11a460335%3A0xcd56ac08f2d4742a!2sCali%2C%20Valle%20del%20Cauca!5e0!3m2!1ses!2sco!4v1629477595693!5m2!1ses!2sco" title="Location" width="100%" height="400" style={{ border: 0}} allowfullscreen="" loading="lazy"></iframe>
         </div>
     )
 }
