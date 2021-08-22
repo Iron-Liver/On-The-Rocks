f09d90dbfe449c265657ad9996b817c4bc5f8990
Drivello <Nico_dd@outlook.com.ar>
2021-08-21 23:37:06 -0300

    WIP- Wheel of Coupons w/ sound

--- client/src/Components/Roulette/cuponRoulette.jsx
+++ client/src/Components/Roulette/cuponRoulette.jsx
@@ -1,48 +1,93 @@
 import React from "react";
-import { motion } from "framer-motion"
-import './cuponRoulette.css'
+import { motion } from "framer-motion";
+import "./cuponRoulette.css";
+import audio from './Wheel_of_cupons.mp3'
 
 export const CuponRoulette = () => {
-
-    const [state,setState] = React.useState(true)
-    const [degrees,setDegrees] = React.useState(0)
-    const [realDegrees,setRealDegrees] = React.useState(0)
+    const [idle, setIdle] = React.useState(true);
+    const [state, setState] = React.useState();
+    const [degrees, setDegrees] = React.useState(0);
+    const cycles = 15 * 360
 
     const variants = {
-        start : {
-            pointerEvents: 'none',
-            // transition: 'all 10s ease-out',
-            transform: `rotate(${degrees}deg)`
+        idle: {},
+        start: {
+            pointerEvents: "none",
+            transform: `rotate(${degrees + cycles}deg)`,
         },
-        end : {
-            pointerEvents: 'auto',
-            // transition: 'none',
-            transform: `rotate(${realDegrees}deg)`
+        end: {
+            pointerEvents: "auto",
+            transform: `rotate(${degrees}deg)`,
+        },
+    };
+    const generateAward = () => {
+        const percent = Math.ceil(Math.random()*100);
+        let discount;
+        console.log(percent);
+        switch (true) {
+            case percent <= 1:
+                discount = 50
+                // 180 = 160 ~ 200
+                setDegrees(Math.floor(Math.random() * (200-160+1)) + 160)
+                break;
+            case percent <= 5:
+                discount = 25
+                // 45 = 25 ~ 65
+                setDegrees(Math.floor(Math.random() * (65-25+1)) + 25)
+                break;
+            case percent <= 15:
+                discount = 20
+                // 225 = 205 ~ 245
+                setDegrees(Math.floor(Math.random() * (245-205+1)) + 205)
+                break;
+            case percent <= 30:
+                discount = 15
+                // 135 = 115 ~ 155
+                setDegrees(Math.floor(Math.random() * (155-115+1)) + 115)
+                break;
+            case percent <= 40:
+                discount = 10
+                // 315 = 295 ~ 335
+                setDegrees(Math.floor(Math.random() * (335-295+1)) + 295)
+                break;
+            case percent <= 75:
+                discount = 5
+                // 270 = 250 ~ 290
+                setDegrees(Math.floor(Math.random() * (290-250+1)) + 250)
+                break;
+            case percent <= 95:
+                discount = 2
+                // 90 = 70 ~ 110
+                setDegrees(Math.floor(Math.random() * (110-70+1)) + 70)
+                break;
+            default:
+                discount = 0
+                // 360 = 340 ~ 20
+                if(Math.random() < 0.5){
+                    setDegrees(Math.floor(Math.random() * (20-0+1)) + 0)
+                }else{
+                    setDegrees(Math.floor(Math.random() * (360-340+1)) + 340)
+                }
+                break;
         }
+        return discount
+    };
+
+    const playAudio = () => {
+        new Audio(audio).play();
     }
 
-    const wheelStart = async (e) => {
-        e.preventDefault()
-        await setDegrees(Math.floor(5000 + Math.random() * 5000))
-        await setTimeout(async() => {
-            await setRealDegrees(degrees % 360)
-            await setState(false);
-            console.log('10s',degrees,realDegrees)
-        }, 15000)
-        await setState(true);
-        console.log(degrees,realDegrees)
-    }
-    
-
-    
-
-    // wheel.addEventListener('transitionend', () => {
-    //     startButton.style.pointerEvents = 'auto';
-    //     wheel.style.transition = 'none';
-    //     const realDegrees = degrees % 360;
-    //     wheel.style.transform = `rotate(${realDegrees}deg)`
-    // })
-
+    const spin = () => {
+        playAudio();
+        const discount = generateAward()
+        setState(true);
+        setIdle(false);
+        setTimeout(async () => {
+            discount ? alert(`Congratulations! You got a %${discount} discount!`) : alert('Thats so sad! Maybe you ll get it another time c:');
+            setState(false);
+            setIdle(true);
+        }, 10000);
+    };
 
     return (
         <div>
@@ -50,8 +95,8 @@ export const CuponRoulette = () => {
             <div className="roulette-container">
                 <motion.img
                     variants={variants}
-                    animate={state ? "start" : "end"}
-                    transition={state ? {duration:10} : {duration:0}}
+                    animate={idle ? "idle" : state ? "start" : "end"}
+                    transition={state ? { duration: 8.897, ease: "easeOut" } : { duration: 2, ease: "easeOut" }}
                     className="roulette"
                     src="https://res.cloudinary.com/dpw5docvm/image/upload/v1629446175/Roulette_cuwthy.png"
                     alt="roulette"
@@ -62,7 +107,18 @@ export const CuponRoulette = () => {
                     alt="roulette_arrow"
                 />
             </div>
-            <input type='button' onClick={wheelStart} className="button" value='Spin'/>
+            <input
+                type="button"
+                onClick={spin}
+                className="button"
+                value="Spin"
+            />
+            <input
+                type="button"
+                onClick={generateAward}
+                className="button"
+                value="Generate"
+            />
         </div>
     );
 };
