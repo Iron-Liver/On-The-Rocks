bc4bd8b499ab40025c33b3a5ac9fde3a93b730c5
Federico <solamentefedee@gmail.com>
2021-08-22 21:57:22 -0300

    Update for Cloudinary

--- client/src/Components/Products/createProductForm.jsx
+++ client/src/Components/Products/createProductForm.jsx
@@ -4,6 +4,7 @@ import { Label, Description, Image, Crop, Fingerprint, MonetizationOn, Category,
 import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
 import Validate from '../../Utils/validate'
 import theme from '../../Utils/theme'
+import CloudinaryUploadWidget from "./productPhotos";
 
 export const useStyles = makeStyles((theme) => ({
 	root: {
@@ -70,177 +71,185 @@ const CreateProductForm = ({ input, setInput, handleSubmit, match, handleDelete 
 	};
 
 	return (
-		<ThemeProvider theme={theme}>
-			<div className='extContCAF'>
-				<form noValidate autoComplete="off" >
-					<h1 className={classes.title}>Create Product</h1>
-					<Grid container direction="column" justifyContent="space-around" alignItems="center" className={`componentDataBox ${classes.root}`} spacing={1}>
-						<Grid >
-							<Grid container spacing={1} alignItems="center">
-								<Grid item >
-									<Label />
+		<>
+			<ThemeProvider theme={theme}>
+				<div className='extContCAF'>
+					<form noValidate autoComplete="off" >
+						<h1 className={classes.title}>Create Product</h1>
+						<Grid container direction="column" justifyContent="space-around" alignItems="center" className={`componentDataBox ${classes.root}`} spacing={1}>
+							<Grid >
+								<Grid container spacing={1} alignItems="center">
+									<Grid item >
+										<Label />
+									</Grid>
+									<Grid item >
+										<TextField
+											error={error["productName"]}
+											helperText={[helperText["productName"]]}
+											id="name"
+											label="Name"
+											name="name"
+											value={input.name}
+											onChange={handleInputChange}
+										/>
+									</Grid>
 								</Grid>
-								<Grid item >
-									<TextField
-										error={error["productName"]}
-										helperText={[helperText["productName"]]}
-										id="name"
-										label="Name"
-										name="name"
-										value={input.name}
-										onChange={handleInputChange}
-									/>
+
+								<Grid container spacing={1} alignItems="center">
+									<Grid item >
+										<Description />
+									</Grid>
+									<Grid item >
+										<TextField
+											error={error["description"]}
+											helperText={[helperText["description"]]}
+											id="description"
+											label="Description"
+											name="description"
+											multiline
+											rows={4}
+											value={input.description}
+											onChange={handleInputChange}
+										/>
+									</Grid>
+								</Grid>
+								<Grid container spacing={1} alignItems="center">
+									<Grid item>
+										<Crop />
+									</Grid>
+									<Grid item>
+										<TextField
+											error={error["size"]}
+											helperText={[helperText["size"]]}
+											id="size"
+											label="Size"
+											name='size'
+											value={input.size}
+											onChange={handleInputChange}
+										/>
+									</Grid>
+								</Grid>
+
+								<Grid container spacing={1} alignItems="center">
+									<Grid item>
+										<Category />
+									</Grid>
+									<Grid item>
+										<TextField
+											error={error["category"]}
+											helperText={[helperText["category"]]}
+											id="category"
+											label="Category"
+											name='category'
+											value={input.category}
+											onChange={handleInputChange}
+										/>
+									</Grid>
+								</Grid>
+
+								<Grid container spacing={1} alignItems="center">
+									<Grid item>
+										<Assistant />
+									</Grid>
+									<Grid item>
+										<TextField
+											error={error["brand"]}
+											helperText={[helperText["brand"]]}
+											id="brand"
+											label="Brand"
+											name='brand'
+											value={input.brand}
+											onChange={handleInputChange}
+										/>
+									</Grid>
+								</Grid>
+
+								<Grid container spacing={1} alignItems="center">
+									<Grid item>
+										<Fingerprint />
+									</Grid>
+									<Grid item>
+										<TextField
+											error={error["sku"]}
+											helperText={[helperText["sku"]]}
+											id="sku"
+											label="Sku"
+											name='sku'
+											value={input.sku}
+											onChange={handleInputChange}
+										/>
+									</Grid>
+								</Grid>
+
+								<Grid container spacing={1} alignItems="center">
+									<Grid item>
+										<MonetizationOn />
+									</Grid>
+									<Grid item>
+										<TextField
+											error={error["price"]}
+											helperText={[helperText["price"]]}
+											id="price"
+											label="Price"
+											name='price'
+											value={input.price}
+											onChange={handleInputChange}
+										/>
+									</Grid>
+								</Grid>
+
+								<Grid container spacing={1} alignItems="center">
+									<Grid item>
+										<Storage />
+									</Grid>
+									<Grid item>
+										<TextField
+											error={error["stock"]}
+											helperText={[helperText["stock"]]}
+											id="stock"
+											label="Stock"
+											name='stock'
+											value={input.stock}
+											onChange={handleInputChange}
+										/>
+									</Grid>
+								</Grid>
+
+								<Grid container spacing={1} alignItems="center">
+									<Grid item>
+										<Image />
+									</Grid>
+									<Grid item>
+										<TextField
+											error={error["image"]}
+											helperText={[helperText["image"]]}
+											id="image"
+											label="Image-URL"
+											name='image'
+											value={input.image}
+											onChange={handleInputChange}
+										/>
+									</Grid>
 								</Grid>
 							</Grid>
 
-							<Grid container spacing={1} alignItems="center">
-								<Grid item >
-									<Description />
-								</Grid>
-								<Grid item >
-									<TextField
-										error={error["description"]}
-										helperText={[helperText["description"]]}
-										id="description"
-										label="Description"
-										name="description"
-										multiline
-										rows={4}
-										value={input.description}
-										onChange={handleInputChange}
-									/>
-								</Grid>
-							</Grid>
-							<Grid container spacing={1} alignItems="center">
+							<Grid container direction="row" justifyContent="center" alignItems="center">
 								<Grid item>
-									<Crop />
-								</Grid>
-								<Grid item>
-									<TextField
-										error={error["size"]}
-										helperText={[helperText["size"]]}
-										id="size"
-										label="Size"
-										name='size'
-										value={input.size}
-										onChange={handleInputChange}
-									/>
-								</Grid>
-							</Grid>
+									<Button style={{ fontWeight: 1000, marginTop: 50 }} color="secondary" onClick={handleSubmit} variant="contained">Add Product</Button>
 
-							<Grid container spacing={1} alignItems="center">
-								<Grid item>
-									<Category />
-								</Grid>
-								<Grid item>
-									<TextField
-										error={error["category"]}
-										helperText={[helperText["category"]]}
-										id="category"
-										label="Category"
-										name='category'
-										value={input.category}
-										onChange={handleInputChange}
-									/>
 								</Grid>
 							</Grid>
-
-							<Grid container spacing={1} alignItems="center">
-								<Grid item>
-									<Assistant />
-								</Grid>
-								<Grid item>
-									<TextField
-										error={error["brand"]}
-										helperText={[helperText["brand"]]}
-										id="brand"
-										label="Brand"
-										name='brand'
-										value={input.brand}
-										onChange={handleInputChange}
-									/>
-								</Grid>
-							</Grid>
-
-							<Grid container spacing={1} alignItems="center">
-								<Grid item>
-									<Fingerprint />
-								</Grid>
-								<Grid item>
-									<TextField
-										error={error["sku"]}
-										helperText={[helperText["sku"]]}
-										id="sku"
-										label="Sku"
-										name='sku'
-										value={input.sku}
-										onChange={handleInputChange}
-									/>
-								</Grid>
-							</Grid>
-
-							<Grid container spacing={1} alignItems="center">
-								<Grid item>
-									<MonetizationOn />
-								</Grid>
-								<Grid item>
-									<TextField
-										error={error["price"]}
-										helperText={[helperText["price"]]}
-										id="price"
-										label="Price"
-										name='price'
-										value={input.price}
-										onChange={handleInputChange}
-									/>
-								</Grid>
-							</Grid>
-
-							<Grid container spacing={1} alignItems="center">
-								<Grid item>
-									<Storage />
-								</Grid>
-								<Grid item>
-									<TextField
-										error={error["stock"]}
-										helperText={[helperText["stock"]]}
-										id="stock"
-										label="Stock"
-										name='stock'
-										value={input.stock}
-										onChange={handleInputChange}
-									/>
-								</Grid>
-							</Grid>
-
-							<Grid container spacing={1} alignItems="center">
-								<Grid item>
-									<Image />
-								</Grid>
-								<Grid item>
-									<TextField
-										error={error["image"]}
-										helperText={[helperText["image"]]}
-										id="image"
-										label="Image-URL"
-										name='image'
-										value={input.image}
-										onChange={handleInputChange}
-									/>
-								</Grid>
-							</Grid>
-
 						</Grid>
-						<Grid container direction="row" justifyContent="center" alignItems="center">
-							<Grid item>
-								<Button style={{ fontWeight: 1000, marginTop: 50 }} color="secondary" onClick={handleSubmit} variant="contained">Add Product</Button>
+					</form>
+				</div>
+			</ThemeProvider>
 
-							</Grid>
-						</Grid>
-					</Grid>
-				</form>
+			<div className="App">
+				<h1>Cloudinary</h1>
+				<h2>Upload Widget</h2>
+				<CloudinaryUploadWidget />
 			</div>
-		</ThemeProvider>
+		</>
 	)
 }
 export default CreateProductForm;
