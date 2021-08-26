1eaac315d81da57630c1b7bdaa5423ff0cae6396
David <david_melo10@yahoo.es>
2021-08-25 21:02:14 -0500

    add chatbot manually

--- client/src/AppGlobal.js
+++ client/src/AppGlobal.js
@@ -33,6 +33,7 @@ import { WheelOfCoupons } from './Components/Game/wheelOfCoupons';
 import { ThemeProvider } from '@material-ui/core';
 import theme from './Utils/theme';
 import Footer from './Components/Footer/footer';
+import ChatBotApp from "./Components/Chatbot/ChatBot"
 
 const AppGlobal = () => {
 
@@ -40,7 +41,8 @@ const AppGlobal = () => {
   return (
       <Router>
         <ThemeProvider theme={theme}>
-          <NavBar />          
+          <NavBar />    
+          <ChatBotApp />       
           <Switch>
             <Route exact path="/" component={landingPage} />            
             <Route exact path="/login" component={LoginUser} />
